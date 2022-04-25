56069a742318b02c43c216834f914fae290f0e6d
Maksim <maksel94@gmail.com>
2022-04-25 22:30:05 +0300

    #string_010

---
+++ src/string/StringNewStringMethods.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class StringNewStringMethods {
+    public static void main(String[] args) {
+        String s = " Hello world!";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
+        System.out.println(s.repeat(2));
+        System.out.println(s.replace('l', 'j'));
+        System.out.println(s.replace("Hello", "Bye"));
+
+        System.out.println(s.substring(6));
+        System.out.println(s.substring(0, 6));
+
+        System.out.println(Arrays.toString(s.split(" ")));
+
+        System.out.println(s.trim());
+        System.out.println(s.strip());
+    }
+}
