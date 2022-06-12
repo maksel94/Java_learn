748f9a911a6bc9c428ddd8530ee43c02663a946e
Maksim <maksel94@gmail.com>
2022-06-12 19:33:22 +0300

    Tic-tac-toe v1

--- src/decomposition/FindPopularWord.java
+++ src/decomposition/FindPopularWord.java
@@ -1,2 +1,18 @@
-package decomposition;public class FindPopularWord {
+package decomposition;
+
+public class FindPopularWord {
+    public static void main(String[] args) {
+        // read source data
+
+        String text = "hello java world java hello I Hello Java World java";
+        // processing
+        String word = findMostPopularInText(text);
+        // display results
+        System.out.println(word);
+    }
+
+    private static String findMostPopularInText(String text) {
+
+        return null;
+    }
 }
